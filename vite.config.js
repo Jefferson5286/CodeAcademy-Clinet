c9f40e5730d76ae355240f6053b5fada4300432a
jeffers <jeffersonlima5286@gmail.com>
2025-03-31 20:00:33 -0300

    Implmentado plugin para suporte a MPA (melhorar organização em desenvolvimento); criado estrutura base da página de registro

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import {defineConfig} from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import viteSimpleMPA from 'vite-simple-mpa/src'
 
 export default defineConfig({
     appType: 'mpa',
-    plugins: [tailwindcss()]
+    plugins: [
+        tailwindcss(),
+        viteSimpleMPA({
+            pages: {
+                leading: {target: './src/pages/leading.html', route: '/'},
+                cursos: {target: './src/pages/cursos.html', route: '/cursos'}
+            }
+        })
+    ]
 });
